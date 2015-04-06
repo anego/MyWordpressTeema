e13e32f674e494bf045547216c335640ca25dba2
anego <anego@project-life.net>
2015-04-06 18:32:13 +0900

    個別記事ページまで疎通

--- ProejctLIFEforClean/loop.php
+++ ProejctLIFEforClean/loop.php
@@ -1,9 +1,16 @@
+<?php
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
 				<div class="post-preview">
 					<a href="<?php the_permalink(); ?>">
 						<h2 class="post-title"><?php the_title(); ?></h2>
 					</a>
 					<p class="post-meta">
-						Posted by <a href="#"><?php echo the_author_nickname(); ?></a> on <?php echo get_the_date(); ?>
+						Posted by <a href="#"><?php the_author(); ?></a> on <?php echo get_the_date(); ?>
 					</p>
 				</div>
 				<hr>
+<?php
+endwhile;
+endif;
+?>
--- ProejctLIFEforClean/single.php
+++ ProejctLIFEforClean/single.php
@@ -1,2 +1,38 @@
-<?php
-// 個別記事ページ
+<?php get_header(); ?>
+
+	<header class="intro-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+					<div class="site-heading">
+						<h1><?php the_title(); ?></h1>
+						<hr class="small">
+						<span class="subheading">Posted by <a href="#"><?php the_author(); ?></a> on <?php echo get_the_date(); ?></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- Main Content -->
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+
+	<?php while (have_posts()) : the_post(); ?>
+	<?php the_content(); ?>
+	<?php endwhile; ?>
+
+				<!-- Pager -->
+				<ul class="pager">
+					<?php
+					if (function_exists("pagination")) {
+						pagination($additional_loop->max_num_pages);
+					}
+					?>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
